4562423d3822c1ba5c05f87e3b616ac4c9ec5346
FNYukii <forestgreen14689@gmail.com>
2026-08-11 00:18:23 +0900

    feat: Sunburst data改善

--- src/charts/recharts/SunburstChart.tsx
+++ src/charts/recharts/SunburstChart.tsx
@@ -1,43 +1,97 @@
 import clsx from 'clsx'
 import { PieChart, Pie, ResponsiveContainer } from 'recharts'
 
-const level1Data = [
-  { name: '国内事業', value: 50, fill: '#3b82f6' },
-  { name: '海外事業', value: 50, fill: '#3b82f6' },
-]
-
-const level2Data = [
-  { name: '関東', value: 25, fill: '#3b82f6' },
-  { name: '関西', value: 25, fill: '#3b82f6' },
-  { name: 'アジア', value: 20, fill: '#3b82f6' },
-  { name: '北米', value: 15, fill: '#3b82f6' },
-  { name: '中東', value: 10, fill: '#3b82f6' },
-  { name: '欧州', value: 5, fill: '#3b82f6' },
-]
-
-const level3Data = [
-  { name: '食品', value: 15, fill: '#3b82f6' },
-  { name: '加工', value: 5, fill: '#3b82f6' },
-  { name: '薬剤', value: 2, fill: '#3b82f6' },
-  { name: '金融', value: 1, fill: '#3b82f6' },
-  { name: '医療', value: 1, fill: '#3b82f6' },
-  { name: '化学', value: 1, fill: '#3b82f6' },
-  // MARK
-  { name: 'none', value: 25, fill: '#0000' },
-  // MARK
-  { name: '食品', value: 10, fill: '#3b82f6' },
-  { name: '洗剤', value: 5, fill: '#3b82f6' },
-  { name: '薬剤', value: 2, fill: '#3b82f6' },
-  { name: '薬剤', value: 1, fill: '#3b82f6' },
-  { name: '薬剤', value: 1, fill: '#3b82f6' },
-  { name: '薬剤', value: 1, fill: '#0000' },
-  // MARK
-  { name: '金融', value: 5, fill: '#3b82f6' },
-  { name: '医療', value: 3, fill: '#3b82f6' },
-  { name: '化学', value: 1, fill: '#3b82f6' },
-  { name: '出版', value: 1, fill: '#3b82f6' },
-  // MARK
-  { name: 'none', value: 20, fill: '#0000' },
+const DATA = [
+  {
+    name: '国内事業',
+    value: 60,
+    fill: '#3b82f6',
+    children: [
+      {
+        name: '関東',
+        value: 40,
+        fill: '#3b82f6',
+        children: [
+          { name: '食品', value: 25, fill: '#3b82f6' },
+          { name: '加工', value: 10, fill: '#3b82f6' },
+          { name: '薬剤', value: 2, fill: '#3b82f6' },
+          { name: '金融', value: 1, fill: '#3b82f6' },
+          { name: '医療', value: 1, fill: '#3b82f6' },
+          { name: '化学', value: 0.5, fill: '#3b82f6' },
+          { name: '興業', value: 0.5, fill: '#3b82f6' },
+        ],
+      },
+      {
+        name: '関西',
+        value: 20,
+        fill: '#3b82f6',
+        children: [
+          { name: '食品', value: 10, fill: '#0000' },
+          { name: '加工', value: 5, fill: '#0000' },
+          { name: '金融', value: 3, fill: '#0000' },
+          { name: '医療', value: 1, fill: '#0000' },
+          { name: '化学', value: 1, fill: '#0000' },
+        ],
+      },
+    ],
+  },
+  {
+    name: '海外事業',
+    value: 40,
+    fill: '#3b82f6',
+    children: [
+      {
+        name: 'アジア',
+        value: 25,
+        fill: '#3b82f6',
+        children: [
+          { name: '食品', value: 10, fill: '#3b82f6' },
+          { name: '加工', value: 3, fill: '#3b82f6' },
+          { name: '金融', value: 3, fill: '#3b82f6' },
+          { name: '薬剤', value: 2, fill: '#3b82f6' },
+          { name: '医療', value: 1, fill: '#3b82f6' },
+          { name: '化学', value: 0.5, fill: '#3b82f6' },
+          { name: '興業', value: 0.5, fill: '#3b82f6' },
+        ],
+      },
+      {
+        name: '北米',
+        value: 5,
+        fill: '#3b82f6',
+        children: [
+          { name: '加工', value: 3, fill: '#0000' },
+          { name: '薬剤', value: 2, fill: '#0000' },
+          { name: '金融', value: 1, fill: '#0000' },
+          { name: '医療', value: 1, fill: '#0000' },
+          { name: '化学', value: 1, fill: '#0000' },
+          { name: '工業', value: 1, fill: '#0000' },
+          { name: '興業', value: 1, fill: '#0000' },
+        ],
+      },
+      {
+        name: '中東',
+        value: 5,
+        fill: '#3b82f6',
+        children: [
+          { name: '食品', value: 2, fill: '#3b82f6' },
+          { name: '加工', value: 2, fill: '#3b82f6' },
+          { name: '薬剤', value: 1, fill: '#3b82f6' },
+        ],
+      },
+      {
+        name: '欧州',
+        value: 5,
+        fill: '#3b82f6',
+        children: [
+          { name: '食品', value: 1, fill: '#3b82f6' },
+          { name: '加工', value: 1, fill: '#3b82f6' },
+          { name: '薬剤', value: 1, fill: '#3b82f6' },
+          { name: '化学', value: 1, fill: '#0000' },
+          { name: '工業', value: 1, fill: '#0000' },
+        ],
+      },
+    ],
+  },
 ]
 
 export const SunburstChart = () => {
@@ -46,10 +100,10 @@ export const SunburstChart = () => {
       <h2 className="text-xl">Sunburst Chart / 階層円グラフ</h2>
 
       <div className={clsx('h-80 aspect-video', 'border border-neutral-400')}>
-        <ResponsiveContainer width="100%" height="100%">
+        <ResponsiveContainer>
           <PieChart>
             <Pie
-              data={level1Data}
+              data={DATA}
               dataKey="value"
               nameKey="name"
               innerRadius={70}
@@ -57,7 +111,7 @@ export const SunburstChart = () => {
             />
 
             <Pie
-              data={level2Data}
+              data={DATA.flatMap((business) => business.children)}
               dataKey="value"
               nameKey="name"
               innerRadius={92}
@@ -65,7 +119,9 @@ export const SunburstChart = () => {
             />
 
             <Pie
-              data={level3Data}
+              data={DATA.flatMap((business) =>
+                business.children.flatMap((area) => area.children),
+              )}
               dataKey="value"
               nameKey="name"
               innerRadius={114}
